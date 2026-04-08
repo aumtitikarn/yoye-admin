0e3db02448c42562c6c3487b473a22abe1475172
Tkkknn_ <aumtitikarn003@gmail.com>
2026-04-09 00:26:21 +0700

    next-config

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,9 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/v1/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
+       source: "/api/v1/:path*",
+        // เติม || "http://localhost:3002" ไว้ข้างหลัง
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3002"}/:path*`,
       },
     ];
   },
